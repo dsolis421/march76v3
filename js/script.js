b1574beb08c3f538d79c518c7a7942b950d3c751
dsolis421 <danny@march76.com>
2018-08-21 21:25:30 -0500

    FANTASTIC start to my new site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 
+  setTimeout(function(){
+    $('#moving-header').animate({"top": "0"}, 1000, "swing",function(){
+      setTimeout(function(){
+        $('.entry-section').fadeIn(1000,"swing");
+        console.log("fadeIn fired");
+      }, 2000);
+    });
+  }, 1000)
 });
