cf033f24ec5a5a73569dfc62c17b1604a50252b1
dsolis421 <danny@march76.com>
2018-08-22 19:23:38 -0500

    POC done, time to integrate PUG

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
   setTimeout(function(){
     $('#moving-header').animate({"top": "0"}, 1000, "swing",function(){
       setTimeout(function(){
-        $('.entry-section').fadeIn(1000,"swing");
+        $('.entry-section, #fadein-footer').fadeIn(1000,"swing");
         console.log("fadeIn fired");
       }, 2000);
     });
-  }, 1000)
+  }, 500)
 });
